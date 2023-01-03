fa0355e3b7ad51b4d86c4f0439115d153fd43d7d
lohcy <loh.cheeyung96@gmail.com>
2023-01-03 13:56:10 -0800

    add spawnEnemy method

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] enemyPrefabs;
+    void Start()
+    {
+        SpawnEnemy(0);
+        SpawnEnemy(1);
+        SpawnEnemy(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy(int enemyIndex)
+    {
+        Instantiate(enemyPrefabs[enemyIndex], new Vector3(0, 0, 10), enemyPrefabs[enemyIndex].transform.rotation);
+    }
+}
