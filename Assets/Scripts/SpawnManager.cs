d1b902f4540ffbfb537a3af1eb3f115f3cec6ad6
lohcy <loh.cheeyung96@gmail.com>
2023-01-18 07:09:08 -0800

    spawn more enemy with corountine

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,12 @@ public class SpawnManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] enemyPrefabs;
+    private int enemyCount = 0;
     void Start()
     {
-        SpawnEnemy(0);
-        SpawnEnemy(1);
+        //SpawnEnemy(0);
+        //SpawnEnemy(1);
+        StartCoroutine(SpawnEnemyWave());
         // SpawnEnemy(2);
     }
 
@@ -19,6 +21,16 @@ public class SpawnManager : MonoBehaviour
         
     }
 
+    IEnumerator SpawnEnemyWave()
+    {
+        
+        while(enemyCount < 15)
+        {
+            yield return new WaitForSeconds(0.5f);
+            SpawnEnemy(Random.Range(0, 2));
+            enemyCount++;
+        }
+    }
     void SpawnEnemy(int enemyIndex)
     {
         Instantiate(enemyPrefabs[enemyIndex], new Vector3(0, 0, 10), enemyPrefabs[enemyIndex].transform.rotation);
