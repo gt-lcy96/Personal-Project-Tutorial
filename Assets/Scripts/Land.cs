2b74313c0e3e7335ac112000aface1c48424d1e6
gt-lcy96 <lohcy96@gmail.com>
2023-01-31 06:42:56 -0800

    map status to material in Land script

--- Assets/Scripts/Land.cs
+++ Assets/Scripts/Land.cs
@@ -4,40 +4,16 @@ using UnityEngine;
 
 public class Land : MonoBehaviour
 {
-    public enum LandStatus
-    {
-        Dirt, tilledLand, watered
-    }
 
-    [SerializeField]
-    private Material dirtMat, tilledLandMat, wateredMat;
-    new Renderer renderer;
-    public LandStatus landStatus;
-
+    // Start is called before the first frame update
     void Start()
     {
-        renderer = GetComponent<Renderer>();
-
-        //set default material for land status
-        SwitchLandStatus(LandStatus.Dirt);
+        
     }
 
     // Update is called once per frame
-    public void SwitchLandStatus(LandStatus status)
+    void Update()
     {
-        landStatus = status;
-        Material material = dirtMat;
-        switch(status)
-        {
-            case LandStatus.Dirt:
-                material = dirtMat;
-                break;
-            case LandStatus.tilledLand:
-                material = tilledLandMat;
-                break;
-            case LandStatus.watered:
-                material = wateredMat;
-                break;
-        }
+        
     }
 }
