fb06f456bca353e5e35417312048f715e765fc21
gt-lcy96 <lohcy96@gmail.com>
2023-02-01 05:24:43 -0800

    create Inventory Manager

---
+++ Assets/Scripts/Manager/InventoryManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public InventoryManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        // Singleton design
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+    }
+
+    [Header("Tools")]
+    [SerializeField] private ItemData[] tools = new ItemData[8];
+    public ItemData equippedTool = null;
+
+    [Header("Items")]
+    [SerializeField] private ItemData[] items = new ItemData[16];
+    public ItemData equippedItem = null;
+
+}
