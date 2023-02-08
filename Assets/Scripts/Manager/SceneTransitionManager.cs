6ef09522401605ee298d50547fda2e98c3864855
gt-lcy96 <lohcy96@gmail.com>
2023-02-08 08:35:20 -0800

    LoadScene

--- Assets/Scripts/Manager/SceneTransitionManager.cs
+++ Assets/Scripts/Manager/SceneTransitionManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransitionManager : MonoBehaviour
 {
@@ -22,5 +23,9 @@ public class SceneTransitionManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    public void SwitchLocation(Location locationToSwitch)
+    {
+        SceneManager.LoadScene(locationToSwitch.ToString());
+    }
     
 }
