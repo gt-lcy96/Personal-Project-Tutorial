7e13b1cb7b182c0b3c6ff4491e3edec991cd9737
gt-lcy96 <lohcy96@gmail.com>
2023-02-08 08:13:48 -0800

    set Scene Transition location (Location match the scene name)

---
+++ Assets/Scripts/Manager/SceneTransitionManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager Instance;
+
+    public enum Location { FarmLand, FarmLand_Test, MyGame }
+
+    private void  Awake()
+    {
+        //SingleTon, if there is more than 1 instance, destroy GameObject
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    
+}
