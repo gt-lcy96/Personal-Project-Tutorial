f74a11229ceb4d304def2122ac3127ec8d07832c
gt-lcy96 <lohcy96@gmail.com>
2023-02-08 21:42:27 -0800

    Add entry point to Home

---
+++ Assets/Scripts/SceneTransition/LocationEntryPoint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationEntryPoint : MonoBehaviour
+{
+    [SerializeField]
+    SceneTransitionManager.Location locationToSwitch;
+    // Start is called before the first frame update
+    
+}
